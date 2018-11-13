e805e756d408f523bb8e23b969743bca77962f91
John Werenski <jtwerenski@gmail.com>
2018-11-13 16:05:10 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,11 +48,8 @@ class Student
     end 
   end 
   
-  def self.new_from_db(array)
-    binding.pry
-    array.map do |row|
-      self.new(row[1], row[2], row[0])
-    end 
+  def self.new_from_db(row)
+    self.new(row[1], row[2], row[0])
   end 
   
   def self.find_by_name(name)
